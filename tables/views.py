74f2ef48be03a40a5dda8e33e5179c454b23c345
Gerardo <emm1912@hotmail.com>
2023-04-16 21:02:40 -0600

    Third commit, adding new files created

--- tables/views.py
+++ tables/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view()
+def tables_list(request):
+    return Response('ok') 
+
+@api_view()
+def tables_item(request, id):
+    return Response(id)
